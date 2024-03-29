71060b6e2c24894cad3854f8c3b865c168901f92
Muhammad Yunus Saifullah <yunus.saiful123@gmail.com>
2024-03-29 22:18:19 +0700

    Day 6

---
+++ aboutString/fungsiPadaString.py
@@ -0,0 +1,22 @@
+a= "Selamat Pagi"
+
+#Memotong string
+print(a[0:2])
+#mengetahui panjang
+print(len(a))
+#menghilangkan white space sebelum dan sesudah karakter string
+print(a.strip())
+#menghasilkan kalimat berhuruf kecil
+print(a.lower())
+#menghasilkan kalimat berhuruf besar
+print(a.upper())
+#menghasilkan kalimat berhuruf besar pada awal kata
+print(a.title())
+#mengganti karakter/kata daalam string
+print(a.replace("Pagi", "Sore"))
+#mencari posisi karakter pada string
+print(a.find("la"))
+#memotong string menjadi list
+print(a.split())
+#mengetahui apakaj semua karakter string adalah angka
+print(a.isnumeric())
---
+++ aboutString/penggunaan.py
@@ -0,0 +1,10 @@
+print('dibatasi tanda petik tunggal')
+print("dibatai tanda petik ganda")
+print()
+print('"petik ganda" dalam petik tunggal')
+print("'petik tunggal' dalam petik ganda")
+print()
+print('\'petik tunggal\' dalam petik tunggal harus menggunakan karakter+escape')
+print("\"petik ganda\" dalam petik ganda harus menggunakan karakter+escape")
+
+
